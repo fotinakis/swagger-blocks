88652431310c205657eadfd01dfb6f7b9ebbd9c7
Mike Fotinakis <mike@fotinakis.com>
2014-09-02 18:38:15 -0700

    Checkpoint, hard work complete around pattern for nodes and evaluation.

--- lib/swagger/rails.rb
+++ lib/swagger/rails.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'swagger/rails/version'
 
 module Swagger::Rails
@@ -5,95 +6,208 @@ module Swagger::Rails
     base.extend(ClassMethods)
   end
 
-  def self.build_json(controller_class)
-    root = {}
-    structures = controller_class.send(:_swagger_datablocks)
+  def self.build_resource_listing_json(resource_listing_class)
+    resource_listing_class.send(:_swagger_nodes)[:resource_listing].as_json
 
-    # Build the simple root keys from the swagger_controller datablock.
-    root.merge!(structures[:controller].data)
+    # resource_listing = {}
+    # # Build the simple resource_listing keys from the swagger_resource_listing node.
+    # resource_listing.merge!(nodes[:resource_listing].data)
 
-    # Build the root "apis" key from each of the swagger_api structures.
-    root[:apis] = {}
-    structures[:apis].each do |api_datablock|
-      api_data = api_datablock.data
-      api_data[:name] = api_datablock.name
+    # # Build the resource_listing "apis" key from each of the swagger_api nodes.
+    # resource_listing[:apis] = {}
+    # nodes[:apis].each do |api_node|
+    #   api_data = api_node.data
+    #   api_data[:name] = api_node.name
 
-      api_datablock.parameters.each do |parameter_datablock|
-        api_data[:parameters] ||= {}
-        api_data[:parameters] = parameter_datablock.data
-      end
+    #   api_node.parameters.each do |parameter_node|
+    #     api_data[:parameters] ||= {}
+    #     api_data[:parameters] = parameter_node.data
+    #   end
 
-      root[:apis].merge!(api_data)
-    end
+    #   resource_listing[:apis].merge!(api_data)
+    # end
+  end
 
-    root
+  def self.build_api_json(resource_class)
   end
 
   module ClassMethods
     private
 
-    def swagger_controller(name, &block)
-      # There should only ever be one of these per controller.
-      datablock = Swagger::Rails::DataBlock.call(name, &block)
-      @swagger_controller_datablock = datablock
+    def swagger_resource_listing(&block)
+      # There is only one of these allowed per object.
+      @swagger_resource_listing_node ||= Swagger::Rails::ResourceListingNode.call(&block)
     end
 
-    def swagger_api(name, &block)
-      datablock = Swagger::Rails::ApiDataBlock.call(name, &block)
-      @swagger_api_datablocks ||= []
-      @swagger_api_datablocks << datablock
+    def swagger_api_root(name, &block)
+      # Each swagger_api declaration appends a new ApiNode.
+      node = Swagger::Rails::ApiNode.call(&block)
+      @swagger_api_nodes ||= []
+      @swagger_api_nodes << node
     end
 
-    def _swagger_datablocks
+    def swagger_api_operation(name, &block)
+      # Each swagger_api declaration appends a new ApiNode.
+      node = Swagger::Rails::Node.call(&block)
+    end
+
+    def _swagger_nodes
       {
-        controller: @swagger_controller_datablock || {},
-        apis: @swagger_api_datablocks || [],
+        resource_listing: @swagger_resource_listing_node || {},
+        apis: @swagger_api_nodes || [],
       }
     end
   end
 
   # -----
 
-  # Dumb data containers for evaluating and holding the originally-definied swagger data.
-  # These objects shouldn't know anything about how to represent the data in its finalized swagger
-  # form so that certain evaluations can be deferred until all objects have been evaluated.
-  class DataBlock
+  # Nodes that represent each object in the Swagger DSL.
+  class Node
     attr_accessor :name
 
-    def self.call(name, &block)
+    def self.call(name: nil, &block)
       # Create a new instance and evaluate the block into it.
       instance = new
       instance.instance_eval(&block)
 
       # Set the first parameter given as the name.
-      instance.name = name
+      instance.name = name if name
       instance
+    end
+
+    def as_json
+      result = {}
+      self.data.each do |key, value|
+        if value.is_a?(Node)
+          result[key] = value.as_json
+        elsif value.is_a?(Array)
+          result[key] = []
+          value.each do |v|
+            result[key] << v.as_json
+          end
+        else
+          result[key] = value
+        end
+      end
+      return result if !name
+      # If "name" is given to this node, wrap the data with a root element with the given name.
+      {name => result}
     end
 
     def data
       @data ||= {}
     end
 
-    def property(key, value)
+    def key(key, value)
+      # raise TypeError.new('key values MUST be strings') if !value.is_a?(String) && !value.is_a?(Bool)
       self.data[key] = value
     end
   end
 
-  class ApiDataBlock < DataBlock
-    def parameters
-      @parameters ||= []
+  # -----
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#51-resource-listing
+  class ResourceListingNode < Node
+    def info(&block)
+      self.data[:info] = InfoNode.call(&block)
     end
 
-    def parameter(name, &block)
-      self.parameters << DataBlock.call(name, &block)
-    end
-
-    def response_messages
-      @response_messages ||= []
-    end
-
-    def response_message(name, &block)
-      self.response_messages << DataBlock.call(name, &block)
+    def authorization(name, &block)
+      self.data[:authorizations] ||= AuthorizationsNode.new
+      self.data[:authorizations].authorization(name, &block)
     end
   end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#514-authorizations-object
+  class AuthorizationsNode < Node
+    def authorization(name, &block)
+      self.data[name] = AuthorizationNode.call(&block)
+    end
+  end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#513-info-object
+  class AuthorizationNode < Node
+    GRANT_TYPES = [:implicit, :authorization_code].freeze
+
+    def scope(&block)
+      self.data[:scopes] ||= []
+      self.data[:scopes] << ScopeNode.call(&block)
+    end
+
+    def grant_type(name, &block)
+      raise ArgumentError.new("#{name} not in #{GRANT_TYPES}") if !GRANT_TYPES.include?(name)
+      self.data[:grantTypes] ||= GrantTypesNode.new
+      self.data[:grantTypes].implicit(&block) if name == :implicit
+      self.data[:grantTypes].authorization_code(&block) if name == :authorization_code
+    end
+  end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#513-info-object
+  class InfoNode < Node; end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#516-scope-object
+  class ScopeNode < Node; end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#517-grant-types-object
+  class GrantTypesNode < Node
+    def implicit(&block)
+      self.data[:implicit] = ImplicitNode.call(&block)
+    end
+
+    def authorization_code(&block)
+      self.data[:authorization_code] = AuthorizationCodeNode.call(&block)
+    end
+  end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#518-implicit-object
+  class ImplicitNode < Node
+    def login_endpoint(&block)
+      self.data[:loginEndpoint] = LoginEndpointNode.call(&block)
+    end
+  end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#5110-login-endpoint-object
+  class LoginEndpointNode < Node; end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#519-authorization-code-object
+  class AuthorizationCodeNode < Node
+    def token_request_endpoint(&block)
+      self.data[:tokenRequestEndpoint] = TokenRequestEndpointNode.call(&block)
+    end
+
+    def token_endpoint(&block)
+      self.data[:tokenEndpoint] = TokenEndpointNode.call(&block)
+    end
+  end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#5111-token-request-endpoint-object
+  class TokenRequestEndpointNode < Node; end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#5112-token-endpoint-object
+  class TokenEndpointNode < Node; end
+
+  # -----
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#42-file-structure
+  class ApiNode < Node
+    def operations(&block)
+      self.data[:operations] ||= []
+      self.data[:operations] << OperationNode.call(&block)
+    end
+  end
+
+  class OperationNode < Node
+    def parameter(name, &block)
+      self.data[:parameters] ||= []
+      self.data[:parameters] << ParameterNode.call(name: name, &block)
+    end
+
+    def response_message(&block)
+      self.data[:responseMessages] ||= []
+      self.data[:responseMessages] << Node.call(&block)
+    end
+  end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#524-parameter-object
+  class ParameterNode < Node; end
 end
--- spec/lib/swagger/rails_spec.rb
+++ spec/lib/swagger/rails_spec.rb
@@ -1,85 +1,192 @@
+require 'json'
 require 'swagger/rails'
 
-class SimpleController
+
+# Some test data directly copied from the swagger docs:
+#
+# https://github.com/wordnik/swagger-codegen/blob/master/src/test/resources/petstore-1.2/api-docs
+RESOURCE_LISTING_JSON = open(File.expand_path('../swagger_resource_listing.json', __FILE__)).read
+# https://github.com/wordnik/swagger-codegen/blob/master/src/test/resources/petstore-1.2/pet
+API_DECLARATION_JSON = open(File.expand_path('../swagger_api_declaration.json', __FILE__)).read
+
+class SimpleObject
   include Swagger::Rails
 
-  swagger_controller :simple do
-    property :apiVersion, '1.0.0'
-    property :swaggerVersion, '1.2'
-    property :basePath, 'http://localhost:8002/api'
-    property :resourcePath, '/pet'
-    property :produces, [
-      'application/json',
-      'application/xml',
-      'text/plain',
-      'text/html',
-    ]
-    property :authorizations, ['oauth2']
+  swagger_resource_listing do
+    key :swaggerVersion, '1.2'
+    key :apiVersion, '1.0.0'
+
+    info do
+      key :title, 'Swagger Sample App'
+      key :description, "This is a sample server Petstore server.  You can find out more about Swagger \n    at <a href=\"http://swagger.wordnik.com\">http://swagger.wordnik.com</a> or on irc.freenode.net, #swagger.  For this sample,\n    you can use the api key \"special-key\" to test the authorization filters"
+      key :termsOfServiceUrl, 'http://helloreverb.com/terms/'
+      key :contact, 'apiteam@wordnik.com'
+      key :license, 'Apache 2.0'
+      key :licenseUrl, 'http://www.apache.org/licenses/LICENSE-2.0.html'
+    end
+
+    authorization :oauth2 do
+      key :type, 'oauth2'
+
+      scope do
+        key :scope, 'email'
+        key :description, 'Access to your email address'
+      end
+
+      scope do
+        key :scope, 'pets'
+        key :description, 'Access to your pets'
+      end
+
+      grant_type :implicit do
+        login_endpoint do
+          key :url, 'http://petstore.swagger.wordnik.com/oauth/dialog'
+        end
+
+        key :tokenName, 'access_token'
+      end
+
+      grant_type :authorization_code do
+        token_request_endpoint do
+          key :url, 'http://petstore.swagger.wordnik.com/oauth/requestToken'
+          key :clientIdName, 'client_id'
+          key :clientSecretName, 'client_secret'
+        end
+
+        token_endpoint do
+          key :url, 'http://petstore.swagger.wordnik.com/oauth/token'
+          key :tokenName, 'access_code'
+        end
+      end
+    end
   end
 
-  swagger_api :show do
-    property :method, 'GET'
-    property :summary, 'Find pet by ID'
-    property :notes, 'Returns a pet based on ID'
-    property :type, :Pet
-    property :nickname, :getPetById
-    property :produces, ['application/json', 'application/xml']
-    property :authorizations, ['oauth2']
+  swagger_api_root :pets do
+    key :path, '/pet'
+  end
 
-    parameter :petId do
-      property :description, 'ID of pet that needs to be fetched'
-      property :required, true
-      property :allowMultiple, false
-      property :type, :string
-      property :paramType, :path
+  swagger_api_operation :pets do
+    key :method, 'GET'
+    key :path, '/pet/{petId}'
+    key :summary, 'Find pet by ID'
+    key :notes, 'Returns a pet based on ID'
+    key :type, :Pet
+    key :nickname, :getPetById
+    key :produces, ['application/json', 'application/xml']
+
+    parameter do
+      key :name, :petId
+      key :description, 'ID of pet that needs to be fetched'
+      key :required, true
+      key :type, :integer
+      key :format, :int64
+      key :paramType, :path
+      key :minimum, '1.0'
+      key :maximum, '100000.0'
     end
 
-    response_message :bad_request do
-      property :message, 'Invalid ID supplied'
-      property :responseModel, 'Pet'
+    response_message do
+      key :code, 400
+      key :message, 'Invalid ID supplied'
+      key :responseModel, 'Pet'
     end
-    response_message :not_found do
-      property :message, 'Pet not found'
+
+    response_message do
+      key :code, 404
+      key :message, 'Pet not found'
     end
   end
+
+  # https://github.com/wordnik/swagger-spec/blob/master/versions/1.2.md#51-resource-listing
+
+  # swagger_api :simple do
+  #   key :apiVersion, '1.0.0'
+  #   key :swaggerVersion, '1.2'
+  #   key :basePath, 'http://localhost:8002/api'
+  #   key :resourcePath, '/pet'
+  #   key :produces, [
+  #     'application/json',
+  #     'application/xml',
+  #     'text/plain',
+  #     'text/html',
+  #   ]
+  #   key :authorizations, ['oauth2']
+  # end
+
+  # swagger_api :show do
+  #   key :method, 'GET'
+  #   key :summary, 'Find pet by ID'
+  #   key :notes, 'Returns a pet based on ID'
+  #   key :type, :Pet
+  #   key :nickname, :getPetById
+  #   key :produces, ['application/json', 'application/xml']
+  #   key :authorizations, ['oauth2']
+  #   parameter :petId do
+  #     key :description, 'ID of pet that needs to be fetched'
+  #     key :required, true
+  #     key :allowMultiple, false
+  #     key :type, :string
+  #     key :paramType, :path
+  #   end
+  #   response_message :bad_request do
+  #     key :message, 'Invalid ID supplied'
+  #     key :responseModel, 'Pet'
+  #   end
+  #   response_message :not_found do
+  #     key :message, 'Pet not found'
+  #   end
+  # end
 end
 
 describe Swagger::Rails do
-  describe 'build_json' do
+  describe 'build_resource_listing_json' do
     it 'outputs the correct data' do
-      data = {
-        apiVersion: '1.0.0',
-        swaggerVersion: '1.2',
-        basePath: 'http://localhost:8002/api',
-        resourcePath: '/pet',
-        produces: [
-          'application/json',
-          'application/xml',
-          'text/plain',
-          'text/html',
-        ],
-        authorizations: ['oauth2'],
-        apis: {
-          method: 'GET',
-          summary: 'Find pet by ID',
-          notes: 'Returns a pet based on ID',
-          type: :Pet,
-          nickname: :getPetById,
-          produces: ['application/json', 'application/xml'],
-          authorizations: ['oauth2'],
-          name: :show,
-          parameters: {
-            description: 'ID of pet that needs to be fetched',
-            required: true,
-            allowMultiple: false,
-            type: :string,
-            paramType: :path,
-          }
-        }
-      }
-      actual = Swagger::Rails.build_json(SimpleController)
-      expect(actual[:apis]).to eq(data[:apis])
+      actual = Swagger::Rails.build_resource_listing_json(SimpleObject)
+      actual = JSON.parse(actual.to_json)  # For access consistency below.
+
+      # Multiple expectations for better test diff output:
+      data = JSON.parse(RESOURCE_LISTING_JSON)
+      expect(actual['info']).to eq(data['info'])
+      expect(actual['authorizations']).to eq(data['authorizations'])
+      expect(actual['apis']).to eq(data['apis'])
       expect(actual).to eq(data)
     end
   end
+  # describe 'build_api_json' do
+  #   it 'outputs the correct data' do
+  #     data = {
+  #       apiVersion: '1.0.0',
+  #       swaggerVersion: '1.2',
+  #       basePath: 'http://localhost:8002/api',
+  #       resourcePath: '/pet',
+  #       produces: [
+  #         'application/json',
+  #         'application/xml',
+  #         'text/plain',
+  #         'text/html',
+  #       ],
+  #       authorizations: ['oauth2'],
+  #       apis: {
+  #         method: 'GET',
+  #         summary: 'Find pet by ID',
+  #         notes: 'Returns a pet based on ID',
+  #         type: :Pet,
+  #         nickname: :getPetById,
+  #         produces: ['application/json', 'application/xml'],
+  #         authorizations: ['oauth2'],
+  #         name: :show,
+  #         parameters: {
+  #           description: 'ID of pet that needs to be fetched',
+  #           required: true,
+  #           allowMultiple: false,
+  #           type: :string,
+  #           paramType: :path,
+  #         }
+  #       }
+  #     }
+  #     actual = Swagger::Rails.build_api_json(SimpleObject)
+  #     expect(actual[:apis]).to eq(data[:apis])
+  #     expect(actual).to eq(data)
+  #   end
+  # end
 end
