72599fa7203e225ae8593776afcec2b1259257c4
Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>
2018-10-08 21:49:16 -0500

    Remove debug puts

--- lib/swagger/blocks/nodes/callback_destination_node.rb
+++ lib/swagger/blocks/nodes/callback_destination_node.rb
@@ -3,7 +3,6 @@ module Swagger
     module Nodes
       class CallbackDestinationNode < Node
         def method(method_name, inline_keys = nil, &block)
-          puts method_name.class
           self.data[method_name] = Swagger::Blocks::Nodes::CallbackMethodNode.call(version: version, inline_keys: inline_keys, &block)
         end
       end
